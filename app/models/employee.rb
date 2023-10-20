30c73f8bd7cc315ca7e96ce5b264eba6578bbdb4
Grant Warkentin <17gwarkentin@gmail.com>
2023-10-19 18:59:20 -0700

    Added full_name method to employee.rb; Added links to every assigned object;

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,9 @@ class Employee < ApplicationRecord
 
   has_many :devices
   has_and_belongs_to_many :softwares, through: :employees_softwares
+
+
+  def full_name
+    first_name + ' ' + last_name
+  end
 end
