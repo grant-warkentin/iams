0bdd92d4faa0cea038dd1e294fdc4fd693d2a0ad
Grant Warkentin <17gwarkentin@gmail.com>
2023-10-27 17:43:06 -0700

    Fixed dashboard view logic;

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
 
   def index
     @devices = Device.all
+    @category = Category.all
+    @software = Software.all
     #@unique_categories = Device.select(:category).distinct
     #@unique_softwares = Software.select(:name, :license_count).distinct
     
